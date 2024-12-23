de843cf692bfeecbd89a7b30fc38dd6e525926c9
flainn1 <vitalasarapulov5@gmail.com>
2024-12-23 23:39:10 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from telebot import types
 
 API_TOKEN = '7579343898:AAEYznuehrWFL2y3VH0fegKAoAF6o3rTqIU'
 CHANNEL_LINK = 'https://t.me/+ScVpIXp3cYpkMjcy'
+SUCCESS_GIF_URL = 'https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWNoa2ZtczZjN3JxcnAybzlybXBidWsxbzJ5azNjY3c4aGR0aG83MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/OjjOPhVoalJLO/giphy.webp'  # Replace with your desired GIF URL
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -14,12 +15,12 @@ def generate_captcha():
 @bot.message_handler(commands=['start'])
 def start(message):
     captcha_word = generate_captcha()
-    keyboard = types.InlineKeyboardMarkup(row_width=3)  # Set row width for two rows
+    keyboard = types.InlineKeyboardMarkup(row_width=3) 
+    
     keyboard.add(types.InlineKeyboardButton(captcha_word, callback_data='captcha_solved'))
     
-    # Generate random words for the other buttons
-    for _ in range(6):
-        random_word = generate_captcha()  # Use the same function to create random words
+    for _ in range(5):  
+        random_word = generate_captcha()
         keyboard.add(types.InlineKeyboardButton(random_word, callback_data='wrong'))
     
     bot.send_message(
@@ -35,6 +36,10 @@ def callback_query(call):
             chat_id=call.message.chat.id,
             message_id=call.message.message_id,
             text="✅ Капча пройдена! Вот ссылка на канал: " + CHANNEL_LINK
+        )
+        bot.send_animation(
+            chat_id=call.message.chat.id,
+            animation=SUCCESS_GIF_URL 
         )
     else:
         bot.edit_message_text(
